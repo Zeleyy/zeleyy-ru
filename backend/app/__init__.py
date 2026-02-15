288b9f7a21a4c4e553fce49bb7bf94a843359bd9
Zeleyy <afuylal@gmail.com>
2026-02-15 18:36:22 +0300

    added valorant dir

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,13 +7,16 @@ from .api import setup_api
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR_NAME = "static"
 GITHUB_DIR_NAME = "github"
+VALORANT_DIR_NAME = "valorant"
 STATIC_DIR = BASE_DIR / STATIC_DIR_NAME
 GITHUB_DIR = STATIC_DIR / GITHUB_DIR_NAME
+VALORANT_DIR = STATIC_DIR / VALORANT_DIR_NAME
 
 
 def init_dirs():
     STATIC_DIR.mkdir(parents=True, exist_ok=True)
     GITHUB_DIR.mkdir(parents=True, exist_ok=True)
+    VALORANT_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def setup_app(app: FastAPI):
