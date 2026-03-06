7665011426e2a52356285055a043efc22f446013
Zeleyy <afuylal@gmail.com>
2026-03-06 21:39:22 +0300

    chore: add react-query-devtools

--- frontend/src/app/providers/query/ui/QueryProvider.tsx
+++ frontend/src/app/providers/query/ui/QueryProvider.tsx
@@ -1,6 +1,7 @@
 import { useState, type ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { createQueryClient } from "@/shared/api";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface QueryProviderProps {
     children: ReactNode;
@@ -13,6 +14,7 @@ export const QueryProvider = (props: QueryProviderProps) => {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false}/>
         </QueryClientProvider>
     );
 };
